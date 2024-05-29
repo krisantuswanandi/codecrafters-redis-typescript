64db2f8ab9e32a99de6c07607a1084e3e8aaeb09
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-05-30 01:40:21 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,9 @@
 import * as net from "net";
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+  connection.on("data", () => {
+    connection.write("+PONG\r\n");
+  });
 });
 
 server.listen(6379, "127.0.0.1");
