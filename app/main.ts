076857d0fb08da05f55d0e3dbf2b736dd325ae3e
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-08-08 22:46:59 +0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,11 @@ import * as net from "net";
 import RESP from "./resp";
 import { handleCommand } from "./commands";
 
+let port = 6379;
+if (process.argv.length > 3 && process.argv[2] === "--port") {
+  port = Number(process.argv[3]);
+}
+
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", async (data) => {
     const result = await handleCommand(data);
@@ -9,4 +14,5 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
   });
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(port, "127.0.0.1");
+console.log(`Server listening on port ${port}`);
