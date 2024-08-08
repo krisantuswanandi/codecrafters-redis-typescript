15c1be24a52ef35b48c625ad2b025ae21b7595db
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-08-08 23:06:43 +0700

    codecrafters submit [skip ci]

--- app/commands/info.ts
+++ app/commands/info.ts
@@ -1,5 +1,5 @@
 import { RespType, RespValue } from "../resp";
 
 export function handler(_cmd: string, _args: string[]): RespValue {
-  return { type: RespType.Bulk, value: "role:master" };
+  return { type: RespType.Bulk, value: "# Replication\nrole:master" };
 }
