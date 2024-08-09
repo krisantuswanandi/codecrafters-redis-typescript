a7beac0188a982e422f7ae66c46444a8e1ae0c7a
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-08-09 23:31:49 +0700

    codecrafters submit [skip ci]

--- app/commands/info.ts
+++ app/commands/info.ts
@@ -3,5 +3,13 @@ import * as info from "../info";
 
 export function handler(_cmd: string, _args: string[]): RespValue {
   const role = info.get("role");
-  return { type: RespType.Bulk, value: `# Replication\nrole:${role}` };
+  const replicationInfo = {
+    role: info.get("role"),
+    master_replid: "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb",
+    master_repl_offset: 0,
+  };
+  const replicationInfoStr = Object.entries(replicationInfo)
+    .map((i) => `${i[0]}:${i[1]}`)
+    .join("\n");
+  return { type: RespType.Bulk, value: `# Replication\n${replicationInfoStr}` };
 }
